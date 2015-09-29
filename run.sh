72335377349816b5a08361b5ae37cadebbc79a4d
Rafael Römhild <rafael@roemhild.de>
2015-09-29 23:31:13 +0200

    Make errbot dirs and config if volume is empty

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 set -e
 
 readonly ERRBIN="/app/venv/bin/err.py"
+readonly ERRRUN="/srv"
 
 
 is_set() {
@@ -11,6 +12,25 @@ is_set() {
 }
 
 
+file_exist() {
+    local file=$1
+
+    [[ -e $file ]]
+}
+
+
+# if mounted volume is empty create dirs
+if [ ! "$(ls -A ${ERRRUN})" ]; then
+    mkdir "${ERRRUN}/data" "${ERRRUN}/plugins" "${ERRRUN}/errbackends"
+fi
+
+
+# copy default container image config file if not exist on volume
+file_exist ${ERRCONF} \
+    || cp /app/config.py ${ERRRUN}
+
+
+# sleep if we need to wait for another container
 if ( is_set ${WAIT} ); then
     echo "Sleep ${WAIT} seconds before starting err..."
     sleep ${WAIT}
